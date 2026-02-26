441e051452e2c819ba4861a2cb265ef43c435c81
Bernoulli0010 <bernoulli0010@github.com>
2026-02-26 06:35:05 +0300

    Add local site files

---
+++ app/config.js
@@ -0,0 +1,6 @@
+window.CONFIG = {
+    PEXELS_API_KEY: "xaKuGpofQUgZYx6JlPEZJdqhgUnsUu8ZpJmbT4tnhA0J2Rpb5vO3ibx0",
+    PIXABAY_API_KEY: "54799067-a3fed06a32d899bc1ede143be",
+    SUPABASE_URL: "https://bjcsbuvjumaigvsjphor.supabase.co",
+    SUPABASE_KEY: "sb_publishable_Ws-ubr-U3Uryo-oJxE0rvg_QTlz2Kqa"
+};
